47244d31a5400441dc443ca9e02dafd9d63d861d
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-10 13:02:12 +0100

    refactored my code, added readme file

---
+++ Readme.md
@@ -0,0 +1,6 @@
+Random Name Generator
+
+I created a simple Random Name Generator in which the user can give a list of names and a number of random names, that hewant to get.
+And gets a list of random names.
+
+All code is present in the main.py file
--- main.py
+++ main.py
@@ -1,23 +1,59 @@
 import random
 
 
-def get_random_names() -> list:
+def get_list_of_users_names() -> list:
     """
-    The function asks the user for a list of names and number and returns random names from this a list
+    The function asks the user to input a list of names, checks and returns it.
     """
-    users_list_of_names = input("Input list of names: ").split()
+    users_input = input("Input list of names separated by comma: ").split(",")
+    users_list_of_names = [name.strip() for name in users_input]
 
     # checking a number of entered names
     while len(users_list_of_names) < 2:
-        users_list_of_names = input("Input more than one name, please: ").split()
-    number_of_random_names = int(input("Input the number of names, that you want to get: "))
+        users_input = input("Input more than one name, please: ").split(",")
+        users_list_of_names = [name.strip() for name in users_input]
 
-    # check a wrong entered number
-    while number_of_random_names >= len(users_list_of_names):
-        print("Wrong number! Try again.")
-        number_of_random_names = int(input("Input the number of names, that you want to get: "))
-
-    return random.choices(users_list_of_names, k=number_of_random_names)
+    return users_list_of_names
 
 
-print(get_random_names())
+def get_number_of_names(list_of_names: list) -> int:
+    """
+    The function asks the user to input a number of names, that he wants to get, checks and returns it.
+    """
+
+    try:
+        user_number = int(input("Input the number of names, that you want to get: "))
+    except ValueError:
+        print("Your number should be an integer! Try again.")
+        user_number = int(input("Input the number of names, that you want to get: "))
+
+    while True:
+        # check a wrong entered number
+        if user_number >= len(list_of_names):
+            print("Your number should be less than the number of names in the list! Try again.")
+            user_number = int(input("Input the number of names, that you want to get: "))
+        elif user_number == 0:
+            print("Your number shouldn't be zero integer! Try again.")
+            user_number = int(input("Input the number of names, that you want to get: "))
+        elif user_number < 0:
+            print("Your number shouldn't  be a negative integer! Try again.")
+            user_number = int(input("Input the number of names, that you want to get: "))
+        else:
+            return user_number
+
+
+
+# print(get_number_of_names(get_list_of_users_names()))
+
+def get_random_names(list_of_names: list, number: int):
+    """
+    The function takes the list of names and number and returns random names from this list
+    """
+
+    print(random.choices(list_of_names, k=number))
+
+
+user_names_list = get_list_of_users_names()
+number = get_number_of_names(user_names_list)
+
+get_random_names(user_names_list, number)
